dc93b9440783f5462053093d18b5f04e689df527
gotoline-one <gotoline-one>
2025-03-16 17:24:00 -0700

    works with java21 on linux

--- .utils/switchToJDK21.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set JAVA_HOME=C:\java\sdk\jdk1.8.0_431
-set PATH=%JAVA_HOME%\bin;%PATH%
-java -version
-pause
---
+++ conway/runmvn.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# run.sh - A script to run conway.jar with JavaFX modules
+
+# Define the jar file and JavaFX module path
+JAR="./target/conway-0.0.1.jar"
+MODULE_PATH="/opt/javafx-sdk/javafx-sdk-21.0.6/lib"
+MODULES="javafx.controls"
+
+
+# Check if the JAR file exists
+if [ ! -f "$JAR" ]; then
+    echo "Error: $JAR not found in the current directory."
+    exit 1
+fi
+
+# Run the jar with the specified module path and modules
+java --module-path "$MODULE_PATH" --add-modules "$MODULES" -jar "$JAR" "$@"
+
